b4712ac69f688a46d8c3f80efdde5987b4fe2d1d
Kruk1 <albertkr@wp.pl>
2023-02-09 19:54:28 +0100

    Add responsive contact design

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { useInView } from 'framer-motion'
+import { motion } from 'framer-motion'
 import './style/contact.css'
 
 function Contact(){
@@ -11,6 +11,7 @@ function Contact(){
             content: ''
         }
     )
+    const [isEmailFormOpen, setIsEmailFormOpen] = useState(false)
 
     function handleChange(event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>)
     {
@@ -24,9 +25,14 @@ function Contact(){
         )
     }
 
+    function openEmailForm()
+    {
+        setIsEmailFormOpen(true)
+    }
+
     return (
         <>
-            <h1>Contact with me!</h1>
+            <h1>Contact</h1>
             <div className="social-media-container">
                 <a href='#' className="social-media linkedin">
                     <i className="icon-linkedin"></i>
@@ -49,20 +55,32 @@ function Contact(){
                     <div className="bg-instagram"></div>
                 </a>
             </div>
-            <form>
-                <i className="icon-mail"></i>
-                <h2>Send email (response time 24h)</h2>
-                <div className="email-container-inputs">
-                    <label htmlFor="name" className='hidden'>Name</label>
-                    <input type="text" name="name" className='email-input' placeholder='Name' onChange={handleChange} />
-                    <label htmlFor="email" className='hidden'>Email</label>
-                    <input type="email" name="email" className='email-input' placeholder='Email (e.g. example@example.com)' onChange={handleChange} />
+            <div className="email-container">
+                <div className="email-form">
+                    {isEmailFormOpen ?                
+                        <motion.form animate={{opacity: 1}}>
+                            <i className="icon-mail"></i>
+                            <h2>Send email (response time 24h)</h2>
+                            <div className="email-container-inputs">
+                                <label htmlFor="name" className='hidden'>Name</label>
+                                <input type="text" name="name" className='email-input' placeholder='Name' onChange={handleChange} />
+                                <label htmlFor="email" className='hidden'>Email</label>
+                                <input type="email" name="email" className='email-input' placeholder='Email (e.g. example@example.com)' onChange={handleChange} />
+                            </div>
+                            <label htmlFor="title" className='hidden'>Title</label>
+                            <input type="text" name="title" id='title' placeholder='Title' onChange={handleChange} />
+                            <textarea name="content" id="content" cols={131} rows={10} placeholder='Write your message here!' onChange={handleChange}></textarea>
+                            <button>Send Email</button>
+                        </motion.form> : 
+                        <div className='open-form' onClick={openEmailForm}>
+                            <i className="icon-mail"></i>
+                            <p>E-mail</p>
+                        </div>
+                    }
+
                 </div>
-                <label htmlFor="title" className='hidden'>Title</label>
-                <input type="text" name="title" id='title' placeholder='Title' onChange={handleChange} />
-                <textarea name="content" id="content" cols={131} rows={10} placeholder='Write your message here!' onChange={handleChange}></textarea>
-                <button>Send Email</button>
-            </form>
+            </div>
+
         </>
     )
 }
